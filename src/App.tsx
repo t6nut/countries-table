88d512a5bba5c9a55dc143c12da236481525c6ba
tonu90 <tonu@comodule.com>
2024-05-26 14:24:48 +0300

    gql fetch data and render

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { useQuery, gql } from '@apollo/client';
 
-function App() {
+const COUNTRIES_QUERY = gql`
+  query Countries {
+    countries {
+      code
+      name
+    }
+  }
+`;
+
+const App: React.FC = () => {
+  const [filter, setFilter] = useState('');
+  const { loading, error, data } = useQuery(COUNTRIES_QUERY);
+
+  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilter(event.target.value);
+  }
+
+  const filteredCountries = data?.countries?.filter((country) => 
+    country.code.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Countries Table</h1>
+      <input 
+        type="text"
+        placeholder="Filter by country code"
+        value={filter}
+        onChange={handleFilterChange} 
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Country Name</th>
+            <th>Country Code</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredCountries?.map((country) => (
+            <tr key={country.code}>
+              <td>{country.name}</td>
+              <td>{country.code}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDom from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, HttpLink, ApolloProvider } from '@apollo/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({ uri: 'https://countries.trevorblades.com/'}),
+});
+
+const root = ReactDom.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
